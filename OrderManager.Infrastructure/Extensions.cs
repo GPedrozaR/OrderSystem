5d1b0d90b5d19b36780b50b83d2e25fb6452d7df
GPedrozaR <gpedrozarodrigues@gmail.com>
2024-09-22 12:43:03 -0300

    feat: Configure infrastructure extensions.

--- OrderManager.Infrastructure/Extensions.cs
+++ OrderManager.Infrastructure/Extensions.cs
@@ -1,6 +1,51 @@
-﻿namespace OrderManager.Infrastructure
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderManager.Infrastructure.MessageBus;
+using RabbitMQ.Client;
+
+namespace OrderManager.Infrastructure
 {
 	public static class Extensions
 	{
+		public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+		{
+			services
+				.AddMessageBus(configuration)
+				.AddDbContext(configuration)
+				.AddHostedService<RabbitMqHostedService>();
+
+			return services;
+		}
+
+		private static IServiceCollection AddMessageBus(this IServiceCollection services, IConfiguration configuration)
+		{
+			var rabbitMqConfig = configuration.GetSection("RabbitMQ");
+
+			var connectionFactory = new ConnectionFactory
+			{
+				HostName = rabbitMqConfig["HostName"],
+				UserName = rabbitMqConfig["UserName"],
+				Password = rabbitMqConfig["Password"]
+			};
+
+			var connection = connectionFactory.CreateConnection(RabbitMqHelper.ConnectionName);
+			services.AddSingleton(connection);
+			services.AddTransient<RabbitMqConsumer>();
+
+			return services;
+		}
+
+		private static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+		{
+			var sqlConfig = configuration.GetSection("SQL");
+
+			services.AddDbContext<OrderManagerDbContext>(
+				options =>
+					options.UseSqlServer(sqlConfig["ConnectionString"]
+				));
+
+			return services;
+		}
 	}
 }
